a6b3fd823fa95fdc47b024680f76ef623cfef657
jarek-mazur <jarekmzr@gmail.com>
2019-03-18 20:02:23 +0100

    finish recursive creating

--- popup.js
+++ popup.js
@@ -4,12 +4,10 @@ let smallerHeader = document.getElementById('smallerHeader');
 
 
 chrome.bookmarks.getSubTree('1', function(array) {
-  console.log('array: ', array)
   takeDirs(array[0])
 })
 
 function takeDirs ({children}) {
-  console.log('childrens: ', children)
   children.forEach((bookmark) => {
     if (bookmark.children) {
     let button = document.createElement('button');
@@ -23,17 +21,30 @@ function takeDirs ({children}) {
 
 }
 
-function onButtonClick(id, parentId, title) {
-  chrome.bookmarks.create({parentId, title: `${title}`}, (newFolder) => {
-    let newOrder = []
+function createBookmars (id, parentId, title, position) {
+  chrome.bookmarks.create({parentId, title, index: position}, (newFolder) => {
     chrome.bookmarks.getChildren(id, (array) => {
       array
       .sort((a,b) => a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1)
       .forEach((bookmark, index) => {
-        newOrder.push({index: index, title: bookmark.title, url: bookmark.url, parentId: newFolder.id})
+        if (bookmark.url) {
+          chrome.bookmarks.create({index: index, title: bookmark.title, url: bookmark.url, parentId: newFolder.id})
+        } else {
+          createBookmars(bookmark.id, newFolder.id, bookmark.title, index);
+        }
       })
-      newOrder.forEach((bookmark) => chrome.bookmarks.create(bookmark)) 
     }) 
-    smallerHeader.innerText = `${title} posortowane!`
-  }) 
+  })
+  smallerHeader.innerText = `${title} posortowane!`
+}
+
+function getChildren (childrenArray) {
+  const helper = [];
+  childrenArray.forEach((bookmark, index) => {
+    helper.push({index: index, title: bookmark.title, url: bookmark.url, parentId: newFolder.id})
+  })
+}
+
+function onButtonClick(id, parentId, title) {
+  createBookmars(id, parentId, title)
 }
